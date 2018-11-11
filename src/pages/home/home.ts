567138b487c2b49b295932958f5bff2014ce7a6c
JhonDrummer <jhondrummer94@gmail.com>
2018-11-11 14:15:52 -0500

    Mi primer consumo

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { UserServiceProvider } from '../../providers/user-service/user-service';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  misUsuarios: any[] = [];
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public myApi: UserServiceProvider) {
+    this.myApi.getUsuarios().subscribe(
+      (data) => {
+        this.misUsuarios = data['results'];
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
   }
 
+  openItem(item: UserServiceProvider) {
+    this.navCtrl.push('DetallePage', {
+      item: item
+    });
+  }
+
+  ionViewDidLoad(){
+
+  }
 }
